98f15c6c11b8e29c8b10d4ed4e951fb7c8e30247
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-06-06 13:22:11 -0300

    Add files via upload

---
+++ src/main/java/com/example/flaggame/screens/title/TitleFragment.kt
@@ -0,0 +1,44 @@
+package com.example.flaggame.screens.title
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.flaggame.R
+import com.example.flaggame.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+    lateinit var binding: FragmentTitleBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        binding.startBtn.setOnClickListener { view:View->
+
+            if (binding.etName.text!!.isNotEmpty()){
+
+                val action = TitleFragmentDirections.actionTitleFragmentToGameFragment()
+                action.name = binding.etName.text.toString()
+                findNavController().navigate(action)
+            } else{
+
+                Toast.makeText(context, "Entre com seu nome", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return  binding.root
+    }
+}
